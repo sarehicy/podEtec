745a9428ff272c615b4a42e8e71d38490a4eb472
Sarah Meicy <133773435+sarehicy@users.noreply.github.com>
2024-10-24 10:58:40 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+navCelular = document.getElementById('nav')
+shadow = document.getElementById('shadow')
+
+function openNav(){
+    navCelular.classList.toggle("none");
+    shadow.classList.toggle('none')
+}
